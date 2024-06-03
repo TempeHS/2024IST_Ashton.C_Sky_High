870f26873db26e7214a55991f7bc48b1fc6d7244
Homsaysyoung <126206739+Homsaysyoung@users.noreply.github.com>
2024-06-04 09:50:15 +1000

    add player controller

---
+++ Pokemon Unity File/Pokemon FINALE/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed; // Declare moveSpeed outside of Start
+
+    private bool isMoving; // Declare isMoving outside of Start
+    private Vector2 input; // Declare input outside of Start
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialization if needed
+        // moveSpeed = 5f; // Example initialization
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
