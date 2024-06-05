5f2d3affb107d09984bd67c40c423d662b831621
Homsaysyoung <126206739+Homsaysyoung@users.noreply.github.com>
2024-06-05 14:15:34 +1000

    commit save

---
+++ Pokemon Unity File/Pokemon FINALE/Assets/Scripts/Pokemon/PokemonBase.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new pokemon")]
+public class Pokemon : ScriptableObject
+
+    // Start is called before the first frame update
+    {
+        [SerializeField] string name;
+        
+        [TextArea]
+        [SerializeField] string description;
+
+        [SerializeField] Sprite frontSprite;
+        [SerializeField] Sprite backSprite;
+
+        [SerializeField] PokemonType type1;
+        [SerializeField] PokemonType type2;
+
+        //Base Stats
+        [SerializeField] int maxHP;
+        [SerializeField] int attack;
+        [SerializeField] int defense;
+        [SerializeField] int spAttack;
+        [SerializeField] int spDefense;
+        [SerializeField] int speed;
+
+    }
+
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+}
