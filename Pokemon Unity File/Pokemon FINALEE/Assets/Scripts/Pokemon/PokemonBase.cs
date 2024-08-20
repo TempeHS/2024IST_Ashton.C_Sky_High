859e6c63a84c13c4de793fc837a66c39d7671ab4
Homsaysyoung <126206739+Homsaysyoung@users.noreply.github.com>
2024-08-20 11:58:00 +1000

    new file new game

---
+++ Pokemon Unity File/Pokemon FINALEE/Assets/Scripts/Pokemon/PokemonBase.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new pokemon")]
+public class PokemonBase : ScriptableObject
+{
+    [SerializeField] string PokemonBasename;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    [SerializeField] int maxHp;
+    [SerializeField] int attack;
+    [SerializeField] int spattack;
+    [SerializeField] int defense;
+    [SerializeField] int spdefense;
+    [SerializeField] int speed;
+
+    // Base Stats
+    public int MaxHp
+    {
+        get { return maxHp; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+    }
+
+    public int SpAttack
+    {
+        get { return spattack; }
+    }
+
+    public int Defense
+    {
+        get { return defense; }
+    }
+
+    public int SpDefense
+    {
+        get { return spdefense; }
+    }
+
+    public int Speed
+    {
+        get { return speed; }
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public string Description
+    {
+        get { return description; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+}
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+}
