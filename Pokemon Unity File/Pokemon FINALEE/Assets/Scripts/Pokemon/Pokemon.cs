e1d1e756a18dd1411da80c171582b3e9f762178a
Homsaysyoung <126206739+Homsaysyoung@users.noreply.github.com>
2024-08-28 14:15:06 +1000

    add new but broken code pls fix

--- Pokemon Unity File/Pokemon FINALEE/Assets/Scripts/Pokemon/Pokemon.cs
+++ Pokemon Unity File/Pokemon FINALEE/Assets/Scripts/Pokemon/Pokemon.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Pokemon
 {
-    private PokemonBase _base; // Made private to follow convention
-    private int level;
+    public PokemonBase Base {get; set;} 
+    public int Level {get; set;} 
 
     public int HP { get; set; }
     public List<Move> Moves { get; set; }
 
     public Pokemon(PokemonBase pBase, int pLevel)
     {
-        _base = pBase;
-        level = pLevel;
-        HP = MaxHp; // Using MaxHp property to initialize HP
+        Base = pBase;
+        Level = pLevel;
+        HP = MaxHp; 
 
         // Generate moves
         Moves = new List<Move>();
-        foreach (var move in _base.LearnableMoves)
+        foreach (var move in Base.LearnableMoves)
         {
-            if (move.Level <= level)
+            if (move.Level <= Level)
             {
                 Moves.Add(new Move(move.MoveBase, move.MoveBase.PP)); // Correctly initialize PP
             }
@@ -34,31 +34,31 @@ public class Pokemon
 
     public int Attack
     {
-        get { return Mathf.FloorToInt(_base.Attack * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.Attack * Level / 100f) + 5; }
     }
 
     public int Defense
     {
-        get { return Mathf.FloorToInt(_base.Defense * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.Defense * Level / 100f) + 5; }
     }
 
     public int SpAttack
     {
-        get { return Mathf.FloorToInt(_base.SpAttack * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.SpAttack * Level / 100f) + 5; }
     }
 
     public int SpDefense
     {
-        get { return Mathf.FloorToInt(_base.SpDefense * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.SpDefense * Level / 100f) + 5; }
     }
 
     public int Speed
     {
-        get { return Mathf.FloorToInt(_base.Speed * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.Speed * Level / 100f) + 5; }
     }
 
     public int MaxHp
     {
-        get { return Mathf.FloorToInt(_base.MaxHp * level / 100f) + 10; }
+        get { return Mathf.FloorToInt(Base.MaxHp * Level / 100f) + 10; }
     }
 }
