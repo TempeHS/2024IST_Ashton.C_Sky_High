7eba3982bf8057bb54bc8dba1dda6dd6659f882c
Homsaysyoung <ashton.chen982@education.nsw.gov.au>
2024-09-25 00:05:16 +1000

    commit

---
+++ Pokemon Unity File/Pokemon FINALEE/Assets/Scripts/Sign.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogActive;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
+        }
+    }
+}
