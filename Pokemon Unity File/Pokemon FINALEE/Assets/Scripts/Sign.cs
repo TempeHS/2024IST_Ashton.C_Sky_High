314984d1f22aada7689e6ac55a9c08469d108728
Homsaysyoung <ashton.chen982@education.nsw.gov.au>
2024-09-25 00:15:06 +1000

    commit and add signs

--- Pokemon Unity File/Pokemon FINALEE/Assets/Scripts/Sign.cs
+++ Pokemon Unity File/Pokemon FINALEE/Assets/Scripts/Sign.cs
@@ -8,14 +8,36 @@ public class Sign : MonoBehaviour
     public GameObject dialogBox;
     public Text dialogText;
     public string dialog;
-    public bool dialogActive;
+    public bool playerInRange;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            Debug.Log("Player in range");
+            playerInRange = true;
         }
     }
 
@@ -23,7 +45,8 @@ public class Sign : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            Debug.Log("Player left range");
+            playerInRange = false;
+            dialogBox.SetActive(false);
         }
     }
 }
